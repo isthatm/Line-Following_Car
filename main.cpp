95b107026e283d102495c9d94e21434d68ce71a3
isthatm <91708157+isthatm@users.noreply.github.com>
2022-12-29 22:26:03 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ WiFiInterface *wifi;
 Ticker servo_ticker;
 Ticker servo_feedback_ticker;
 
-Thread carThread;
+Thread carThread(osPriorityHigh);
 EventQueue car_queue;
 
 Thread measureThread;
@@ -32,68 +32,87 @@ volatile int message_num = 0;
 volatile int arrivedcount = 0;
 volatile bool closed = false;
 
-const char *topic = "Car status"; // TODO "Mbed"
+const char *topic = "Car"; 
 
-int pattern;
-int a = 0; int task = 0; int subTask = 0; int taskConfirm = 0; int confirmed;
-float d0, d1, inita0, inita1;
-float d; float s;
+int pattern; double objD;
+int a; 
+int objectDetection = 1; // there is no object ahead by default
+float inita0 = 0; float inita1 = 0;
+double d,s;
+
+
 const float PI = 3.13159;
-const float R = 10.2; //distance between wheels -> radius of rotaion
-const float rotP = 64.08849013;
-float obj[2]; float rotAngle[2];
-float w1, w2;
-float w = 0; 
-double objD;
+//const float R = 5.1; //distance between wheels -> radius of rotaion
+const float rotP = 32.04424507;
+float obj[2], w[2];
+int i = 0;
+int rotatingSpeed = 40; int carSpeed = 32;
 
-
-void carStatus(){// task = 1, task = 2, task = 4
+void carStatus(){
             //distance travelled
+            double d0, d1;
             d0 = 6.5*PI*(abs(car.servo0.angle-inita0)/360);
             d1 = 6.5*PI*(abs(car.servo1.angle-inita1)/360);
             s = ( (d0+d1) / 2 ); // speed
-            //printf("angle0: %d,  angle1: %d", car.servo0.angle, car.servo1.angle);
-            //printf("111 Distance travelled from last task: %f\n", d[task]); 
-            //printf("Distance travelled: %f, Task executed: %d", d, task);
             inita0 = car.servo0.angle; //update current wheel angle for next call
             inita1 = car.servo1.angle;
-            d = d + s;    
+            d = d + s;   
+            //printf("Distance: %.2f, Speed: %.2f, Width: %.2f \n", d, s, w[0] + w[1] );
 }
 
-void obsMeasure2(){
-   float p2, theta2, dis2, alpha2, rotAngle2;
-   double obsMeasureAngle0;
+void widthCalculation (double d, double currentWheelAngle, double initWheelAngle ){
+    float wheelAngle, rotAngle, p, theta;
+    wheelAngle =  abs(currentWheelAngle) - abs(initWheelAngle);
+    p = 6.5*PI*( abs(rotAngle) / 360 );
+    theta = (p / rotP)*360; //
+    //alpha1 = 90 - theta1;
+    rotAngle = (theta*PI) / 180; // deg to rad
+    w[i] = sin(rotAngle)*d;
+    i++;
+}
 
+void obsMeasure(){
+    double d1, d2, obsMeasureAngle0;
+    obsMeasureAngle0 = car.servo1.angle;
     while(1) {
-        pattern = (int)qti1;
-        if (pattern == 0b0110){
-            obsMeasureAngle0 = car.servo1.angle; //- rotAngle[0];
-        }
-        dis2 = (float)ping1;
-        if (dis2 > 20){
-            //printf("d2: %f\n", dis2);
-            car.turn(-45,0.1);
+        d1 = (float)ping1;
+        if (d1 > 20){
+            car.rotate(rotatingSpeed);
         }
         else{
-            obj[1] = dis2;
             car.stop();
-            //printf("Distance to object 2: %f\n", obj[1]);
             break;
         }
         ThisThread::sleep_for(10ms);
     }
-
-    rotAngle[1] = abs(car.servo1.angle) - abs(obsMeasureAngle0); 
-    p2 = 6.5*PI*( abs( rotAngle[1] ) / 360);
-    theta2 = (p2 / rotP)*360; //
-    alpha2 = 90 - theta2;
-    rotAngle2 =(90 - alpha2)*PI / 180;
-    w2 = abs(sin(rotAngle2)*obj[1]);
-    w = w1 + w2;
-    //printf("Distance to object 2: %f , w2: %f, wheel_angle: : %d, p2: %f, alpha2: %f, rotAngle2: %f\n", obj[1], w2, abs(car.servo1.angle) + 20, p2, alpha2, rotAngle2);
-    //printf("Distance to object 2: %f\n", obj[1]);
-    //printf("Rotangle: %f, initial angle: %f, current angle: %d\n", rotAngle[1], obsMeasureAngle0, car.servo1.angle);
-    //printf("Distance between 2 objects: %f\n", w1+w2);
+    widthCalculation(d1, car.servo1.angle, obsMeasureAngle0);
+    
+    // 2nd object
+    while(1) {
+        pattern = (int)qti1;
+        if (pattern == 0b0110){
+            car.stop();
+            ThisThread::sleep_for(1s);
+            obsMeasureAngle0 = car.servo1.angle; 
+            break;
+        }
+        else {
+            car.rotate(-rotatingSpeed);
+        }
+    }
+    while(1){
+        d2 = (float)ping1;
+        if (d2 > 20){
+            car.rotate(-rotatingSpeed);
+        }
+        else{
+            car.stop();
+            break;
+        }
+        ThisThread::sleep_for(10ms);
+    }
+    
+    widthCalculation(d2 , car.servo1.angle, obsMeasureAngle0);
     
     while(1){
        pattern = (int)qti1;
@@ -102,51 +121,73 @@ void obsMeasure2(){
            break;
        }
        else{
-           car.turn(40, 0.1);
+           car.rotate(35);
        }
    }
-   task++; //task = 3 -> not acutually a task, just a mark to indicate that TASK 2 has been done
-   //distance_travelled(task, 1); //CALL HERE
-   // to run the car again
+   if ( w[0] + w[1] > 15){
+       objectDetection = 1; // the gap length is wide enough, it's safe to go
+    }
 }
 
-void obsMeasure1(){
-    float p1, theta1, dis1, alpha1, rotAngle1;
-    double obsMeasureAngle0;
-    obsMeasureAngle0 = car.servo1.angle;
-    while(1) {
-        dis1 = (float)ping1;
-        if (dis1 > 20){
-            //printf("d1: %f\n", dis1);
-            car.turn(40,0.1);
-        }
-        else{
-            obj[0] = dis1;
-            car.stop();
-            break;
-        }
-        ThisThread::sleep_for(10ms);
-   }
-    rotAngle[0] =  abs(car.servo1.angle) - abs(obsMeasureAngle0);
-    p1 = 6.5*PI*( abs(rotAngle[0]) / 360);
-    theta1 = (p1 / rotP)*360; //
-    alpha1 = 90 - theta1;
-    rotAngle1 = (90 - alpha1)*PI / 180;
-    w1 = sin(rotAngle1)*obj[0];
-    //printf("Distance to object 1: %f , w1: %f wheel_angle: %f\n", obj[0], w1, rotAngle[0]);
-    //printf("Distance to object 1: %f \n", obj[0]);
-    car.turn(-45,0.1);
-    ThisThread::sleep_for(2s);
-    measure_queue.call(&obsMeasure2);   
+void carDriving(){
+      pattern = (int)qti1;
+      //printf("pattern: %d", pattern);
+      objD = (float)ping1;
+      if (objD < 11.5){ 
+          objectDetection = 0; // car enters cautious mode
+          car.stop();
+          ThisThread::sleep_for(1s);
+          car.rotate(rotatingSpeed);
+          ThisThread::sleep_for(2s);
+          while(1) {
+              pattern = (int)qti1;
+              if (pattern == 0b0110){
+                  car.stop();
+                  ThisThread::sleep_for(1s);
+                  break;
+                }
+              else {
+                  car.rotate(rotatingSpeed);
+                }
+            }
+            measure_queue.call(&obsMeasure);
+      }
+      if (objectDetection){
+          switch (pattern) {
+            case 0b1000: car.turn(carSpeed, -0.1); a = 0; break;
+            case 0b1100: car.turn(carSpeed, -0.4); a = 0; break;
+            case 0b0100: car.turn(carSpeed, -0.7); a = 0; break;
+            case 0b0110: car.goStraight(carSpeed); a = 0; break;
+            case 0b0010: car.turn(carSpeed, 0.7); a = 0; break;
+            case 0b0011: car.turn(carSpeed, 0.4); a = 0; break;
+            case 0b0001: car.turn(carSpeed, 0.1); a = 0; break;
+            case 0b0111: { // TASK 1: turning left
+                if (a==0){
+                    car.stop();
+                    a++;
+                    ThisThread::sleep_for(1s); 
+                    break;
+                }
+                else if(a==1){
+                    car.turn(30, 0.1);
+                    ThisThread::sleep_for(1s); 
+                    break;
+                }
+            }
+            case 0b1111: { // FINISHED
+                car.stop();
+                break;
+            }
+          }
+      }
 }
-
 
 void messageArrived(MQTT::MessageData& md) {
     MQTT::Message &message = md.message;
     char msg[300];
     sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
     //printf(msg);
-    ThisThread::sleep_for(2000ms);
+    ThisThread::sleep_for(1s);
     char payload[300];
     sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
     //printf(payload);
@@ -156,7 +197,7 @@ void messageArrived(MQTT::MessageData& md) {
 void publish_message(MQTT::Client<MQTTNetwork, Countdown> *client1) {
   MQTT::Message message;
   char buff[100];
-  sprintf(buff,"Distance travelled: %f (cm), Speed: %f (cm/s), Task executed: %d,  Object width: %f ", d, s, task, w); // client prints the message if confirmed = 1
+  sprintf(buff,"Distance travelled: %.2f (cm), Speed: %.2f(cm/s), Width: %.2f ", d, s, w[0]+ w[1]);
   message.qos = MQTT::QOS0;
   message.retained = false;
   message.dup = false;
@@ -165,13 +206,12 @@ void publish_message(MQTT::Client<MQTTNetwork, Countdown> *client1) {
   int rc = client1->publish(topic, message);
 
   printf("rc:  %d\r\n", rc);
-  printf("Pubished message: %s\r\n", buff);
+  printf("[Pubished]: %s\r\n", buff);
 }
 
 void close_mqtt() { 
     closed = true; 
 }
-
 
 int main() {
 
@@ -194,21 +234,22 @@ int main() {
   MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
   // TODO: revise host to your IP
-  const char *host = "192.168.43.84";
+  //const char *host = "192.168.137.1"; //pc
+  const char *host = "192.168.43.84"; // phone
   const int port=1883;
   printf("Connecting to TCP network...\r\n");
   printf("address is %s/%d\r\n",host, port); // check setting
 
   int rc = mqttNetwork.connect(host, port); //(host, 1883);
   if (rc != 0) {
-    printf("Connection error.");// -3011: open mqtt mosquitto broker
+    printf("Connection error.");// -3011: restart mqtt mosquitto broker
     return -1;
   }
   printf("Successfully connected!\r\n");
 
   MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
   data.MQTTVersion = 3;
-  data.clientID.cstring = "Car status"; // TODO Mbed
+  data.clientID.cstring = "Mbed"; 
 
   if ((rc = client.connect(data)) != 0) {
     printf("Fail to connect MQTT\r\n");
@@ -216,130 +257,37 @@ int main() {
 
   if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
             printf("Fail to subscribe\r\n");
-  }
+  }   
 
-   // ******* CAR DRIVING *******
+// *********************
+
   carThread.start(callback(&car_queue, &EventQueue::dispatch_forever));
   measureThread.start(callback(&measure_queue, &EventQueue::dispatch_forever));
-  car_queue.call_every(5s, &publish_message, &client);
+  car_queue.call_every(10ms, &carDriving);
   car_queue.call_every(1s, &carStatus);
-    
-  inita0 = car.servo0.angle;
-  inita1 = car.servo1.angle;
+  car_queue.call_every(1s, &publish_message, &client);
 
-  car.goStraight(40);
-    while(1) {
-
-        if (task == 4){
-            //distance_travelled(task, 1); // CALL HERE
-            break;
-        }
-
-       pattern = (int)qti1;
-       //printf("pattern: %d\n",pattern);
-
-       if (task==1 && subTask == 1){
-           objD = (float)ping1;
-           //printf("Distance to object: %f", objD);
-           if (objD < 11.5){ 
-               //TASK 2: obstacle detetection, prev 13.5
-               int rotaionCounter = 0;
-               car.stop();
-               while(1){
-                   car.rotate(40);
-                    if (rotaionCounter == 0){
-                        ThisThread::sleep_for(1s);
-                        rotaionCounter++;
-                    }
-                    int rotatingPattern;
-                    rotatingPattern = (int)qti1;
-                    //printf("rotating pattern: %d\n", rotatingPattern);
-                    if (rotatingPattern == 6){
-                        car.stop();
-                        break;
-                    }
-                    ThisThread::sleep_for(10ms);
-                }
-                task++; // task = 2
-                //distance_travelled(task, 1);
-                //subTask++; // subTask = 3
-                ThisThread::sleep_for(2s);
-                car_queue.call(&obsMeasure1);     
-            }
-        }
-       if (task != 2) { // if (task != 2) -> stop the car when excuting obstacle detection
-        switch (pattern) {
-            case 0b1000: car.turn(33, -0.1); a = 0; break;
-            case 0b1100: car.turn(33, -0.4); a = 0; break;
-            case 0b0100: car.turn(30, -0.7); a = 0; break;
-            case 0b0110: car.goStraight(30); a = 0; break;
-            case 0b0010: car.turn(30, 0.7); a = 0; break;
-            case 0b0011: car.turn(33, 0.4); a = 0; break;
-            case 0b0001: car.turn(33, 0.1); a = 0; break;
-            case 0b0111: car.turn(33,0.3); a = 0; break; // special case for unflattened sections
-            case 0b1001: car.turn(33,0.4); a = 0; break;
-            case 0b1111: {
-                if (a==0 && task == 3){
-                    car.stop();
-                    task++; //task = 4 (finished)
-                    break;
-                }
-                else if (a==0 && task == 0) { // TODO: change a to 0
-                    car.stop(); 
-                    a++; task++;// task = 1 
-                    ThisThread::sleep_for(1s);
-                    break;
-                }
-                if(a==1 && task == 1){ //TASK 1: turn to the left branch
-                    car.turn(31, 0.1);
-                    ThisThread::sleep_for(1s);
-                    //subTask++; 
-                    break;
-                }
-
-                if (task == 1 && subTask == 0){
-                    car.goStraight(30);
-                    subTask++; // subTask = 1: turn on laserPing
-                    break;
-                }
-                /*
-                else if(a==1 && task == 4){
-                    car.stop();
-                    break;
-                }
-                */
-            
-                else { // make car go again 
-                    car.goStraight(30);
-                    break;
-                }
-            }
-            default: car.goStraight(28);
-        }
-     }
-     ThisThread::sleep_for(10ms);
-    }
-    // ******* END *******
-
-    while (1) {
+// **********************
+  while (1) {
         if (closed) break;
-    client.yield(500);
-    ThisThread::sleep_for(1s);
+        client.yield(500);
+        ThisThread::sleep_for(1s);
     }
   
-  printf("Ready to close MQTT Network......\n");
+    printf("Ready to close MQTT Network......\n");
 
-  if ((rc = client.unsubscribe(topic)) != 0) {
-    printf("Failed: rc from unsubscribe was %d\n", rc);
-  }
-  if ((rc = client.disconnect()) != 0) {
-    printf("Failed: rc from disconnect was %d\n", rc);
-  }
+    if ((rc = client.unsubscribe(topic)) != 0) {
+        printf("Failed: rc from unsubscribe was %d\n", rc);
+    }
+    if ((rc = client.disconnect()) != 0) {
+        printf("Failed: rc from disconnect was %d\n", rc);
+    }
 
-  mqttNetwork.disconnect();
-  printf("Successfully closed!\n");
+    mqttNetwork.disconnect();
+    printf("Successfully closed!\n");
 
-  return 0;
+    return 0;
+
 }
 
 
